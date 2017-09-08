b20f1f9a0158d0004c64aa0acac09d1fbb31d265
symbios <dxeqtr@gmail.com>
2017-09-08 14:09:51 +0300

    -

--- src/main/java/ru/namibios/arduino/Transfer.java
+++ src/main/java/ru/namibios/arduino/Transfer.java
@@ -21,6 +21,8 @@ public class Transfer implements Runnable{
 
 	private static final int EVERY_HOUR = 1000 * 60 * 60;
 
+	private Task task;
+	
 	private SubTasker subTasker;
 	
 	public Transfer() {
@@ -37,8 +39,6 @@ public class Transfer implements Runnable{
 		logger.info("Start...");
 		DelayUtils.delay(3000);
 		
-		Task task = null;
-		
 		if(!Property.portInstance().isOpen()) {
 			logger.info("Port is closed. Exit");
 			System.exit(1);
@@ -46,77 +46,81 @@ public class Transfer implements Runnable{
 		
 		logger.info("Port is open...");
 			
-			switch (Process.getInstance()) {
-				case START:{
-					logger.info("Starting fish... ");
+		switch (Process.getInstance()) {
+			case START:{
+				logger.info("Starting fish... ");
+				
+				Region fixedRegion = new FixedKey(ArduinoSubTask.space);
+				
+				task = new Task(fixedRegion, 3000, 5000);
+				task.run();
+				
+				logger.info("Check sub-task...");
+				subTasker.check();
+				
+				break;
+			}
+			
+			case WAIT_FISH: {
+				logger.info("Wait fish...");
+				try {
+					Region waitFish = new Region(ImageType.SPACE);
 					
-					Region fixedRegion = new FixedKey(ArduinoSubTask.space);
-					
-					task = new Task(fixedRegion, 3000, 5000);
+					task = new Task(waitFish, 3000, 0);
 					task.run();
 					
-					logger.info("Check sub-task...");
-					subTasker.check();
-					
-					break;
+				}catch (Exception e) {
+					 logger.error("Exception " + e);
 				}
-				case WAIT_FISH: {
-					logger.info("Wait fish...");
-					try {
-						Region waitFish = new Region(ImageType.SPACE);
-						
-						task = new Task(waitFish, 3000, 0);
-						task.run();
-					}catch (Exception e) {
-						 logger.error("Exception " + e);
-					}
-					break;
-				}
-				case PARSE_LINE:{
-					logger.info("Cut the fish...");
-					try {
-						Region parseLine = new FixedKey(ArduinoSubTask.space);
-						
-						task = new Task(parseLine, 1200, 0);
-						task.run();
-						
-					}catch (Exception e) {
-						logger.error("Exception " + e);
-					}
-					break;
-				}
-				case PARSE_KAPCHA:{
-					logger.info("Parsing kapcha...");
-					
-					try{
-						Region kapcha = new Kapcha();
-						
-						task = new Task(kapcha, 3920, 10000);
-						task.run();
-						
-					}catch (Exception e) {
-						logger.error("Exception " + e);
-						DelayUtils.delay(7000);
-						Process.initStart();
-					}
-					break;
-				}
-				case FILTER_LOOT:{
-					logger.info("Check loot...");
-					
-					try {
-						Region filter = new FishLoot();
-						
-						task = new Task(filter, 5000, 0);
-						task.run();
-						
-					}catch (Exception e) {
-						logger.error("Exception " + e);
-					}
-					
-					break;
-				} 	
+				break;
 			}
+			case PARSE_LINE:{
+				logger.info("Cut the fish...");
+				try {
+					Region parseLine = new FixedKey(ArduinoSubTask.space);
+					
+					task = new Task(parseLine, 1200, 0);
+					task.run();
+
+				}catch (Exception e) {
+					logger.error("Exception " + e);
+				}
+				break;
+			}
+		
+			case PARSE_KAPCHA:{
+				logger.info("Parsing kapcha...");
+				
+				try{
+					Region kapcha = new Kapcha();
+					
+					task = new Task(kapcha, 3920, 10000);
+					task.run();
+					
+				}catch (Exception e) {
+					logger.error("Exception " + e);
+					DelayUtils.delay(7000);
+					Process.initStart();
+				}
+				break;
+			}
+			
+			case FILTER_LOOT:{
+				logger.info("Check loot...");
+				
+				try {
+					Region filter = new FishLoot();
+					
+					task = new Task(filter, 5000, 0);
+					task.run();
+					
+				}catch (Exception e) {
+					logger.error("Exception " + e);
+				}
+				
+				break;
+			} 	
+		}
 			
 		Property.portInstance().closePort();
 		logger.info("Port closed...");
